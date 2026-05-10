2491dde958bc1e0d8aad9c1fb8403be8a99846c2
PaulArias27 <paularias2727@gmail.com>
2026-05-09 21:12:31 -0500

    PA: se implemento la clase vendedor

---
+++ src/main/java/com/krakedev/examen/modelo/Vendedor.java
@@ -0,0 +1,57 @@
+package com.krakedev.examen.modelo;
+
+public class Vendedor {
+    private String cedula;
+    private int numeroVentas;
+    private double sueldoFijo;
+    private double comisionPorVenta;
+    private String tipo;
+
+    public Vendedor(String cedula, String tipo) {
+        this.cedula = cedula;
+        this.tipo = tipo;
+    }
+
+    public double calcularSueldo() {
+        return sueldoFijo;
+    }
+
+    // Getters y Setters
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public int getNumeroVentas() {
+        return numeroVentas;
+    }
+
+    public void setNumeroVentas(int numeroVentas) {
+        this.numeroVentas = numeroVentas;
+    }
+
+    public double getSueldoFijo() {
+        return sueldoFijo;
+    }
+
+    public void setSueldoFijo(double sueldoFijo) {
+        this.sueldoFijo = sueldoFijo;
+    }
+
+    public double getComisionPorVenta() {
+        return comisionPorVenta;
+    }
+
+    public void setComisionPorVenta(double comisionPorVenta) {
+        this.comisionPorVenta = comisionPorVenta;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Cedula: " + cedula + " Tipo: " + tipo;
+    }
+}
