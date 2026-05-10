d3367d4ac041ec9ffb19e0518a6627b2f4ae4aeb
PaulArias27 <paularias2727@gmail.com>
2026-05-09 21:51:50 -0500

    PA: se agrego ejemplo de nullpointerexception

---
+++ src/main/java/com/krakedev/examen/test/testNull.java
@@ -0,0 +1,10 @@
+package com.krakedev.examen.test;
+
+import com.krakedev.examen.modelo.Vendedor;
+
+public class testNull {
+    public static void main(String[] args) {
+        Vendedor v = null;
+        System.out.println(v.getCedula());
+    }
+}
